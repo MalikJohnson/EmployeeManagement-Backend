b297a2544608ece60bc6f5ad4981e45a4ddd9966
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-02-18 12:36:12 -0500

    Initial commit

---
+++ src/main/java/com/employeeManagement/model/Employee.java
@@ -0,0 +1,11 @@
+package com.employeeManagement.model;
+
+public class Employee {
+	private Long id;
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phone;
+	private String imageUrl;
+	private String employeeCode;
+}
