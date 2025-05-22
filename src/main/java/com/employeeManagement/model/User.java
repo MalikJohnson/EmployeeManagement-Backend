8efb9e7f9e854b8fce58fa58030a28c3e80f8fc9
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-05-21 23:55:02 -0400

    Changed to postgres and changed application properties to use environment variables  for production
    Now using environment variables for application properties. Switched to postgres to save production space in ram. Didn't want to spin up a mysql container. Also changed table name due to conflict in postgres. User is a reserved keyword in postgres but not mysql. So I changed table name to users using at table.

--- src/main/java/com/employeeManagement/model/User.java
+++ src/main/java/com/employeeManagement/model/User.java
@@ -14,8 +14,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
 	public User(Long id, String username, String email, String password, String role, List<Employee> employees) {
 		this.id = id;
